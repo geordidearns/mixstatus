d21e4d707532946723f5d26b0ed4164519ef1f90
geordi <geordidearns@gmail.com>
2024-11-29 08:55:00 +0100

    Remove abstraction layer for search input

--- dashboard/src/components/services-wrapper.tsx
+++ dashboard/src/components/services-wrapper.tsx
@@ -4,22 +4,23 @@ import { useDebounceValue } from "usehooks-ts";
 import { SearchInput } from "./search-input";
 import { ServicesTable } from "./services-table";
 import { useState } from "react";
+import { Input } from "./ui/input";
 
 export function ServicesWrapper() {
 	const [searchInput, setSearchInput] = useState<string>("");
-	const [debouncedValue] = useDebounceValue(searchInput.trim(), 300);
+	const [debouncedValue] = useDebounceValue(searchInput, 500);
 
 	return (
 		<div className="space-y-8">
-			<SearchInput
-				value={searchInput}
-				onChange={(value: string) => {
-					setSearchInput(value); // Remove the trim() here
-				}}
-				onClear={() => {
-					setSearchInput("");
-				}}
-			/>
+			<div className="sm:max-w-[400px]">
+				<Input
+					placeholder="Find a service..."
+					type="search"
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value.trim())}
+					className="rounded-full p-6 pr-8 bg-card focus:border-indigo-600 focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:outline-none"
+				/>
+			</div>
 			<ServicesTable searchValue={debouncedValue} />
 		</div>
 	);
