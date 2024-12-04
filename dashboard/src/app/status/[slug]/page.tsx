4f5ad82c5f00708095d5f82c63c7d49b97a2e5f2
geordi <geordidearns@gmail.com>
2024-12-04 08:05:47 +0100

    Add codemod to await params on dynamic page

--- dashboard/src/app/status/[slug]/page.tsx
+++ dashboard/src/app/status/[slug]/page.tsx
@@ -1,20 +1,22 @@
 interface ServicePageProps {
-	params: {
+	params: Promise<{
 		slug: string;
-	};
-	searchParams: { [key: string]: string | string[] | undefined };
+	}>;
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-export default function ServicePage({ params }: ServicePageProps) {
-	return (
+export default async function ServicePage(props: ServicePageProps) {
+    const params = await props.params;
+    return (
 		<div className="container py-8">
 			<h1 className="text-2xl font-bold">Service: {params.slug}</h1>
 		</div>
 	);
 }
 
-export async function generateMetadata({ params }: ServicePageProps) {
-	return {
+export async function generateMetadata(props: ServicePageProps) {
+    const params = await props.params;
+    return {
 		title: `Is ${params.slug} down? See the latest status on ${params.slug} here`,
 		description: `If ${params.slug} is having issues, monitor the latest status changes or outages here.`,
 	};
